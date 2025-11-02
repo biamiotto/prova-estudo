a0c5f7eec16ca808649901a6e2e6dd9d23962fad
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-11-02 20:51:34 -0300

    estudando pra prova af

---
+++ src/routes/comidasRoute.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as ComidasController from './../controllers/comidasController.js';
+
+const router = Router();
+
+router.get("/", ComidasController.listarTodos);
+router.get("/:id", ComidasController.listarUm);
+router.post("/", ComidasController.criar);
+router.delete("/:id", ComidasController.deletar);
+router.put("/:id", ComidasController.atualizar);
+
+export default router;
